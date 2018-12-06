af1e5b4e680176225d043aa6fc78109e46dce442
Adham <admuaq@gmail.com>
2018-12-06 16:49:00 +0000

    changed variable

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,5 @@
 class Group < ApplicationRecord
-attr_encrypted_options.merge!(key: ENV.fetch('PWD'))
+attr_encrypted_options.merge!(key: '/Users/macbook/Development')
   attr_encrypted :ssn
   attr_encrypted :app
 end
