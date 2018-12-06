56cd5e8dc4eed606663cda6c77ce092935f61b15
Adham <admuaq@gmail.com>
2018-12-06 17:04:07 +0000

    changed variable

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,5 @@
 class Group < ApplicationRecord
-attr_encrypted_options.merge!(key: '/Users/macbook/Development')
+attr_encrypted_options.merge!(key: 'test/v1/Users/macbook/Development')
   attr_encrypted :ssn
   attr_encrypted :app
 end
